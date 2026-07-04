8b4945b69fb93d1bd311210e932eca63aa2ee1bd
warstatus29-ctrl <warstatus29@gmail.com>
2026-07-04 16:00:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-from config import BOT_TOKEN
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "✅ Rajasthan Medical Officer Quiz Bot is Online!"
-    )
+    await update.message.reply_text("✅ Rajasthan Medical Officer Quiz Bot is Online!")
 
-app = Application.builder().token(BOT_TOKEN).build()
+async def main():
+    app = Application.builder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("start", start))
 
-print("Bot Started...")
-app.run_polling()
+    print("Bot Started...")
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+    await app.updater.idle()
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
